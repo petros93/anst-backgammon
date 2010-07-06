719a2a20d324d340427672770a1bb32a6bc8835a
andrei.penchev <andrei.penchev@3aff1d39-aab4-e29f-8f7a-1b32b948a22e>
2010-07-06 15:03:46 +0000

    initial version

---
+++ trunk/src/backgammonator/interfacce/game/Constants.java
@@ -0,0 +1,32 @@
+package backgammonator.interfacce.game;
+
+/**
+ * Common constants.
+ */
+public interface Constants {
+	
+	/**
+	 * Number of points on the board.
+	 */
+	int POINTS_COUNT = 24;
+	
+	/**
+	 * Number of checkers with the same color.
+	 */
+	int CHECKERS_COUNT = 15;
+	
+	/**
+	 * Number of dice.
+	 */
+	int DICE_COUNT = 2;
+	
+	/**
+	 * Number of dice on double.
+	 */
+	int DOUBLE_DICE_COUNT = 4;
+	
+	/**
+	 * Maximum number to be on dice.
+	 */
+	int MAX_DIE = 6;
+}
---
+++ trunk/src/backgammonator/interfacce/game/Dice.java
@@ -0,0 +1,22 @@
+package backgammonator.interfacce.game;
+
+/**
+ * Creates new randomly generated pair of dice.
+ */
+public interface Dice {
+
+	/**
+	 * Returns the number on the first die.
+	 */
+	public int getDie1();
+
+	/**
+	 * Returns the number on the second die.
+	 */
+	public int getDie2();
+
+	/**
+	 * Checks if the dice form double.
+	 */
+	public boolean isDouble();
+}
