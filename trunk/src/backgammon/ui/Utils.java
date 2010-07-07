0931b04145317a76ffd29b145ebbb362ea3e4d76
andrei.penchev <andrei.penchev@3aff1d39-aab4-e29f-8f7a-1b32b948a22e>
2010-07-07 15:14:07 +0000

    fixes

--- trunk/src/backgammon/ui/Utils.java
+++ trunk/src/backgammon/ui/Utils.java
@@ -25,27 +25,27 @@ public class Utils {
 	}
     
 	public static void creatCoordinates() {
-		coord = new PointImpl[25][6];
+		coord = new PointImpl[25][11];
 		for (int i = 13; i <= 18; i++) {
-			for (int j = 1; j <= 5; j++) {
+			for (int j = 1; j <= 10; j++) {
 				coord[i][j] = new PointImpl(25 + ((i - 13) * 56), 15 + ((j - 1) * 40));
 			}
 		}
 
 		for (int i = 19; i <= 24; i++) {
-			for (int j = 1; j <= 5; j++) {
+			for (int j = 1; j <= 10; j++) {
 				coord[i][j] = new PointImpl(465 + ((i - 19) * 56), 15 + ((j - 1) * 40));
 			}
 		}
 
 		for (int i = 12; i >= 7; i--) {
-			for (int j = 1; j <= 5; j++) {
+			for (int j = 1; j <= 10; j++) {
 				coord[i][j] = new PointImpl(25 - ((i - 12) * 56), 440 - ((j - 1) * 40));
 			}
 		}
 		
 		for (int i = 6; i >= 1; i--) {
-			for (int j = 1; j <= 5; j++) {
+			for (int j = 1; j <= 10; j++) {
 				coord[i][j] = new PointImpl(465 - ((i - 6) * 56), 440 - ((j - 1) * 40));
 			}
 		}
