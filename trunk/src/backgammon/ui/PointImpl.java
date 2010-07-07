6dbf0deb2d1f4487d9cbc25b3fa6e0106377202f
andrei.penchev <andrei.penchev@3aff1d39-aab4-e29f-8f7a-1b32b948a22e>
2010-07-07 17:48:52 +0000

    fixes

---
+++ trunk/src/backgammon/ui/PointImpl.java
@@ -0,0 +1,20 @@
+package backgammon.ui;
+
+class PointImpl {
+
+	private int x, y;
+
+	public PointImpl(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
